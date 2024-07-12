b1b42e190448bfa0b413c4c3aa3138649b0834bd
arpit-evo <arpit.m@evolutioncloud.in>
2024-07-12 19:24:33 +0530

    feat: create route  for  accepting messages or not

---
+++ src/app/api/accept-messages/route.ts
@@ -0,0 +1,48 @@
+import dbConnect from "@/lib/dbConnect";
+import { getServerSession, User } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/options";
+import UserModel from "@/models/User";
+
+export const POST = async (request: NextRequest) => {
+  await dbConnect();
+
+  try {
+    const session = await getServerSession(authOptions);
+    const user: User = session?.user as User;
+
+    if (!session || !session.user) {
+      return NextResponse.json(
+        { success: false, message: "Not authenticated" },
+        { status: 401 }
+      );
+    }
+
+    const userId = user._id;
+    const { acceptMessages } = await request.json();
+
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      userId,
+      {
+        isAcceptingMessages: acceptMessages,
+      },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Unable to find user to update message acceptance status",
+        },
+        { status: 404 }
+      );
+    }
+  } catch (error) {
+    console.error("Error updating message acceptance status:", error);
+    return NextResponse.json(
+      { success: false, message: "Error updating message acceptance status" },
+      { status: 500 }
+    );
+  }
+};
